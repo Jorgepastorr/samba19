03947a42f5e4781db1b4c54f86629aa4feb8967a
Jorge Pastor <jorge.pastor.rr@gmail.com>
2019-11-29 21:41:36 +0100

    añado usuarios a install

--- samba19:base/install.sh
+++ samba19:base/install.sh
@@ -9,4 +9,15 @@ cp /opt/docker/* /var/lib/samba/public/.
 mkdir /var/lib/samba/privat
 #chmod 777 /var/lib/samba/privat
 cp /opt/docker/*.md /var/lib/samba/privat/.
+
 cp /opt/docker/smb.conf /etc/samba/smb.conf
+
+useradd lila
+useradd rock
+useradd patipla
+useradd pla 
+
+echo -e "lila\nlila" | smbpasswd -a lila
+echo -e "rock\nrock" | smbpasswd -a rock
+echo -e "patipla\npatipla" | smbpasswd -a patipla
+echo -e "pla\npla" | smbpasswd -a pla
