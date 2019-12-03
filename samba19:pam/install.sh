fb64ff52ccda71edbf51aaf8c25bf06dfc761670
Jorge Pastor <jorge.pastor.rr@gmail.com>
2019-12-03 16:50:36 +0100

    cambio sed or cut

--- samba19:pam/install.sh
+++ samba19:pam/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+# configuracion de samba
 mkdir /var/lib/samba/public
 chmod 777 /var/lib/samba/public
 mkdir /var/lib/samba/privat
@@ -8,7 +8,7 @@ mkdir /var/lib/samba/privat
 cp /opt/docker/smb.conf /etc/samba/smb.conf
 
 
-
+# usuarios locales samba
 useradd lila
 useradd rock
 useradd patipla
@@ -20,15 +20,15 @@ echo -e "patipla\npatipla" | smbpasswd -a patipla
 echo -e "pla\npla" | smbpasswd -a pla
 
 
-
+# usuarios ldap samba
 for num in {01..08}
 do 
     echo -e "jupiter\njupiter" | smbpasswd -a user$num 
 
     liniaget=$( getent passwd user$num )
-    uid=$( echo $liniaget | sed -r 's/^([^:]*:){2}([^:]*):.*$/\2/' )
-    gid=$( echo $liniaget | sed -r 's/^([^:]*:){3}([^:]*):.*$/\2/' )
-    homedir=$( echo $liniaget | sed -r 's/^([^:]*:){5}([^:]*):.*$/\2/' )
+    uid=$( echo $liniaget | cut -d: -f 3 )
+    gid=$( echo $liniaget | cut -d: -f 4 )
+    homedir=$( echo $liniaget | cut -d: -f 6 )
 
     if [ ! -d $homedir ];then
         mkdir -p $homedir
