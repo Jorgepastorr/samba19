8cff0de34535c7c67fa28606003e82c0f45dc29a
jorge Pastor <jorge.pastor.rr@gmail.com>
2019-12-05 12:30:31 +0100

    retoque docker samba

--- samba19:pam/install.sh
+++ samba19:pam/install.sh
@@ -37,3 +37,12 @@ do
     fi
 
 done
+
+
+useradd local1
+useradd local2
+useradd local3
+
+echo 'local1' | passwd local1 --stdin
+echo 'local2' | passwd local2 --stdin
+echo 'local3' | passwd local3 --stdin
